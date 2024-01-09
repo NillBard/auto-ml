c22fc0a31fc07556c2670b67250b96c80dc67c62
Alesya Moseva <mosevaa13@gmail.com>
2024-01-09 19:39:45 +0300

    add menu and main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,21 @@
 import { lazy } from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { Flex } from '@chakra-ui/react'
+import Menu from '../components/commonMenu'
+import Main from './main'
 
 const PipelinePage = lazy(() => import('./createPipeline'))
 const TablePage = lazy(() => import('./historyTable'))
 
 export default function Routing() {
   return (
-    <>
+    <Flex>
+      <Menu />
       <Routes>
+        <Route path={'/'} element={<Main />} />
         <Route path={'/table'} element={<TablePage />} />
         <Route path={'/pipeline'} element={<PipelinePage />} />
       </Routes>
-    </>
+    </Flex>
   )
 }
