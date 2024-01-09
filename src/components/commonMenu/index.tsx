e55b71c19abbda0aa8bf2e41dce4d53a58b8c1f2
Alesya Moseva <mosevaa13@gmail.com>
2024-01-09 23:35:56 +0300

    done menu router

--- src/components/commonMenu/index.tsx
+++ src/components/commonMenu/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router-dom'
+
 import { Button, Flex, Image, VStack } from '@chakra-ui/react'
 
 import statsIcon from '../../assets/icons/IoBarChartOutline.svg'
@@ -5,18 +7,34 @@ import terminalIcon from '../../assets/icons/IoTerminalOutline.svg'
 import deployIcon from '../../assets/icons/BsFillDiagram3Fill.svg'
 
 const Menu = () => {
+  const navigate = useNavigate()
   return (
     <Flex height="100vh" width="170px" bg="gray.200">
       <VStack pl="10px" pt="25px">
-        <Button width="150px" variant="ghost" justifyContent="flex-start">
+        <Button
+          width="150px"
+          variant="ghost"
+          justifyContent="flex-start"
+          onClick={() => navigate('/')}
+        >
           <Image src={statsIcon} pr="10px" />
           Главная
         </Button>
-        <Button width="150px" variant="ghost" justifyContent="flex-start">
+        <Button
+          width="150px"
+          variant="ghost"
+          justifyContent="flex-start"
+          onClick={() => navigate('/training')}
+        >
           <Image src={terminalIcon} pr="10px" />
           Обучение
         </Button>
-        <Button width="150px" variant="ghost" justifyContent="flex-start">
+        <Button
+          width="150px"
+          variant="ghost"
+          justifyContent="flex-start"
+          onClick={() => navigate('/pipeline')}
+        >
           <Image src={deployIcon} pr="10px" />
           Деплой
         </Button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,19 @@ import { lazy } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Flex } from '@chakra-ui/react'
 import Menu from '../components/commonMenu'
-import Main from './main'
 
 const PipelinePage = lazy(() => import('./createPipeline'))
 const TablePage = lazy(() => import('./historyTable'))
+const MainPage = lazy(() => import('./main'))
 
 export default function Routing() {
   return (
     <Flex>
       <Menu />
       <Routes>
-        <Route path={'/'} element={<Main />} />
-        <Route path={'/table'} element={<TablePage />} />
-        <Route path={'/pipeline'} element={<PipelinePage />} />
+        <Route path={'/'} element={<MainPage />} />
+        <Route path={'/training'} element={<TablePage />} />
+        <Route path={'/pipeline/*'} element={<PipelinePage />} />
       </Routes>
     </Flex>
   )
